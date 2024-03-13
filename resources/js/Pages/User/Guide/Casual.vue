beb3a0cce2d56058efe21cdc76fca491086150d5
Adnan Tinte Chohan <adti.chohan.up@phinmaed.com>
2024-03-14 04:40:17 +0800

    fixed casual design, overdid it a bit will tone it down later since josh is salty by design

--- resources/js/Pages/User/Guide/Casual.vue
+++ resources/js/Pages/User/Guide/Casual.vue
@@ -1,90 +1,86 @@
 <template>
   <UserLayout>
 
-    <section class="bg-white dark:bg-slate-900 relative pt-16">
-    <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-12 relative z-10">
-        <div class="relative rounded-s bg-transparent dark:bg-gray-900 shadow-lg p-2 md:p-10">
-            <div class="bg-gray-100 dark:bg-gray-800 rounded-lg opacity-80 absolute inset-0"></div>
-            <div class="px-8 py-4 relative">
-                <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl text-fuchsia-800">
-                    Step 1
-                </h1>
-                <!-- Adjust the image to fill the container side to side -->
-                <img class="mt-4 w-full lg:mt-10 mb-6 rounded-lg" src="https://image.made-in-china.com/202f0j00YzLoINnJwybS/Cheap-Glass-Computer-Gaming-PC-Case-with-Cooling-Fan-Transparent-Side-Panel.webp" alt="office content 2" style="width: 726px; height: 520px; object-fit: contain;">
-                <h3 class="mb-4 text-2xl tracking-tight font-extrabold text-gray-900 dark:text-white">Recommended PC for Gamers: Brands and Builds</h3>
-            </div>
-        </div>
+    <div class="mt-16 text-center pt-16">
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl text-fuchsia-800">
+            How to get started on building your PC!
+        </h1>
     </div>
-</section>
 
-
-
+  
 
     <section class="bg-white dark:bg-gray-900">
   <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
       <div class="max-w-screen-md mb-8 lg:mb-16">
-          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Recommended PC for Gamers: Brands and Builds</h2>
-          <p class="text-gray-500 sm:text-xl dark:text-gray-400">Here at PC-Nubbies, You can check some in-depth guides on what are the recommended, as well as check out shop to buy those parts</p>
+          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Introduction for PC Casuals</h2>
+          <p class="text-gray-500 sm:text-xl dark:text-gray-400">Here are what you will need to take note of for starting your PC journey as a newbie. Check out the 3 Essentials below.</p>
       </div>
       <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
           <div>
               <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                 <v-icon left>mdi-book-open-blank-variant-outline</v-icon>
-              </div>
-              <h3 class="mb-2 text-xl font-bold dark:text-white">Casual</h3>
-              <p class="text-gray-500 dark:text-gray-400"> We guide you through the crucial decisions of choosing the key components that will define your PC's performance. Delve into the interplay of parts, ensuring compatibility and suitability for your desired tasks, whether gaming, productivity, or creative work.</p>
-          </div>
-          <div>
-              <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                <v-icon left>mdi-tag-check-outline</v-icon>  
-              </div>
-              <h3 class="mb-2 text-xl font-bold dark:text-white">Gaming</h3>
-              <p class="text-gray-500 dark:text-gray-400">You can expect to find a wide range of components within an average price range. This is a balance between quality and affordability, ensuring that you can build a PC that meets your needs without breaking the bank.</p>
-          </div>
-          <div>
-              <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                <v-icon left>mdi-cart-outline</v-icon>                
+                 <v-icon left>mdi mdi-magnify</v-icon>
               </div>
               <h3 class="mb-2 text-xl font-bold dark:text-white">Compatibility</h3>
-              <p class="text-gray-500 dark:text-gray-400">To enhance the user experience, we've made it easier for visitors to navigate to category pages from product pages. This simplification not only improves the flow of information but also makes it easier for users to discover new products or explore different brands within our categories</p>
+              <p class="text-gray-500 dark:text-gray-400"> 
+When choosing PC parts, ensure compatibility by checking motherboard support for the CPU, RAM, and GPU, along with power supply and case fit. This prevents compatibility issues, ensuring a smooth build process.</p>
+          </div>
+          <div>
+              <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                <v-icon left>mdi-cash-multiple</v-icon>  
+              </div>
+              <h3 class="mb-2 text-xl font-bold dark:text-white">Budgeting</h3>
+              <p class="text-gray-500 dark:text-gray-400">Determine your budget and prioritize components accordingly. Allocate a larger portion to essential components like the CPU and GPU.</p>
+          </div>
+          <div>
+              <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                <v-icon left>mdi-tools</v-icon>                
+              </div>
+              <h3 class="mb-2 text-xl font-bold dark:text-white">Compatibility</h3>
+              <p class="text-gray-500 dark:text-gray-400"> Ensure compatibility between components, especially the CPU and motherboard socket type, RAM compatibility with the motherboard, and the physical size of components fitting within the case.</p>
           </div>
       </div>
   </div>
 </section>
 
+<div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12 relative z-10">
+            <div class="relative rounded-s bg-transparent dark:bg-gray-900 shadow-lg p-2 md:p-10 flex items-center justify-center">
+                <div class="bg-gray-100 dark:bg-gray-800 rounded-lg opacity-80 absolute inset-0"></div>
+                <div class="px-8 py-4 relative">
+                  <iframe width="1000" height="515" src="https://www.youtube.com/embed/ExxFxD4OSZ0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+
+
 <section class="bg-white dark:bg-gray-900">
-  <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
-      <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Step 1</h2>
-            <p class="mb-4">We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.</p>
-            <p>We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick.</p>
-        </div>
-        <div class="grid grid-cols-1 gap-4 mt-8">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/5cdwLtGJIk8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        </div>r 
+
+  <div class="mt-16 text-center pt-16">
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl text-fuchsia-800">
+            Here are some of the popular computer builds atm
+        </h1>
     </div>
+  
     <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
       <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Computer 1, Budget $1000-$2000</h2>
+            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Mid Range PC Build around 650$.</h2>
             <p class="mb-4"><ul>
-              <li>meow</li>
+              <li>CPU: Intel Core i5-7500 </li>
+              <li>Cooler: Cooler Master Hyper 212 Evo</li>
+              <li>Graphics card: MSI GeForce GTX 1070</li>
+              <li>Motherboard: Asus Prime H270-Pro</li>
+              <LI>RAM: G.SKILL Aegis 16GB (2x8GB) DDR4 clocked at 2133MHz</LI>
+              <LI>SSD: SanDisk SD8SBAT-256G-1122 Z400s 256GB SSD</LI>
+              <li>HDD: Seagate Desktop HDD ST2000DM001 2TB</li>
+              <li>Case: Fractal Design Define R5</li>
+              <li>PSU: EVGA SuperNOVA 550 G2</li>
             </ul></p>
-            <p>We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick.</p>
+            <p></p>
         </div>
         <div class="grid grid-cols-1 gap-4 mt-8">
-            <img class="mt-4 w-full lg:mt-10 rounded-lg" src="https://image.made-in-china.com/202f0j00YzLoINnJwybS/Cheap-Glass-Computer-Gaming-PC-Case-with-Cooling-Fan-Transparent-Side-Panel.webp" alt="office content 2">
+            <img class="mt-4 w-full lg:mt-10 rounded-lg" src="https://www.gamespot.com/a/uploads/scale_super/1568/15683559/3148018-fractal%20design%20define%20r5.jpg" alt="office content 2">
         </div>
     </div>
-    <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
-        <div class="grid grid-cols-1 gap-4 mt-8">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/5cdwLtGJIk8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        </div>
-        <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Step 1</h2>
-            <p class="mb-4">We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.</p>
-            <p>We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick.</p>
-        </div>
-    </div>
+   
 </section>
 
     <Related></Related>
